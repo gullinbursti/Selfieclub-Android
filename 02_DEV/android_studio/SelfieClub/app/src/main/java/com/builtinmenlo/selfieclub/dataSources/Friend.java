7617c396b8d6432ac80259a94afabe772c27910a
Leonardo Ortiz <Leonardo@Leonardos-MacBook-Pro.local>
2014-06-19 19:20:11 -0600

    Added calls for friends request

---
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/dataSources/Friend.java
@@ -0,0 +1,16 @@
+package com.builtinmenlo.selfieclub.dataSources;
+
+/**
+ * Created by Leonardo on 6/19/14.
+ */
+public class Friend extends User {
+    private int state;
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+}
--- 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/models/UserFriendsProtocol.java
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/models/UserFriendsProtocol.java
@@ -1,5 +1,6 @@
 package com.builtinmenlo.selfieclub.models;
 
+import com.builtinmenlo.selfieclub.dataSources.FriendsViewData;
 import com.builtinmenlo.selfieclub.dataSources.User;
 
 import java.util.ArrayList;
@@ -8,7 +9,7 @@ import java.util.ArrayList;
  * Created by Leonardo on 6/16/14.
  */
 public interface UserFriendsProtocol {
-    public void didReceiveFriendsList(ArrayList<User> friendsList);
+    public void didReceiveFriendsList(FriendsViewData friendsViewData);
     public void didReceiveFriendsListError(String errorMessage);
 
 }
