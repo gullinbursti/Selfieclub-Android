e95fc4a9f30191d586c057c14b364edae524c210
Javy <javiercz@gmail.com>
2014-06-24 00:53:51 -0600

    Match Friends Screen to Comps

--- 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/fragments/FriendsTabBtnFragment.java
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/fragments/FriendsTabBtnFragment.java
@@ -24,34 +24,35 @@ import android.app.Fragment;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.BitmapShader;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Shader;
+import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.builtinmenlo.selfieclub.R;
-import com.builtinmenlo.selfieclub.dataSources.ActivityItem;
 import com.builtinmenlo.selfieclub.dataSources.Friend;
 import com.builtinmenlo.selfieclub.dataSources.FriendsViewData;
 import com.builtinmenlo.selfieclub.dataSources.User;
 import com.builtinmenlo.selfieclub.models.UserFriendsProtocol;
 import com.builtinmenlo.selfieclub.models.UserManager;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
 
-import java.net.URL;
+import java.io.InputStream;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -60,9 +61,9 @@ import java.util.List;
 
 
 // <[!] class delaration [¡]>
-public class FriendsTabBtnFragment extends Fragment implements UserFriendsProtocol{
+public class FriendsTabBtnFragment extends Fragment implements UserFriendsProtocol {
     public ListView lv;
-    public List<Friend> friends;
+    public ArrayList<Friend> friends;
     public User owner;
     private MyCustomAdapter adapter;
     private ProgressBar loadingIcon;
@@ -72,10 +73,10 @@ public class FriendsTabBtnFragment extends Fragment implements UserFriendsProtoc
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         //]~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~._
         UserManager userManager = new UserManager();
-        userManager.requestFriends(this,"2394","+17143309754|+15617164724|+15617164831|+17409726939|+16505754720|+14086210100|+15303041380|+141 52646152|+16192042875|+16179397216|+15612369460|+18475305634|+14089215625|+19783175663|+14159354255|+14255032279|+16502244999|+14159028877|+12393709811|+12 135098296|+14083109905|+19196560716|+14087181396|+14085069784|+14152549391|+16509969693|+15612719211|+17869735014|+15624539140|+16506192664|+13107078969|+1 4152609007|+16263750676|+14079219866|+18187266581|+18312220104|+19312496956|+13105978645|+13106262124|+19493502980|+14084995523|+14153788924|+17816161660|+ 14156867366|+17816161660|+16507434738|+14153368700|+15105797189|+14153086768|+16506192665|+12068566868|+13105005463|+13035266649|+12133009127|+14156095557| +14084293828|+16507968877|+12135905745");
+        userManager.requestFriends(this, "2394", "+17143309754|+15617164724|+15617164831|+17409726939|+16505754720|+14086210100|+15303041380|+141 52646152|+16192042875|+16179397216|+15612369460|+18475305634|+14089215625|+19783175663|+14159354255|+14255032279|+16502244999|+14159028877|+12393709811|+12 135098296|+14083109905|+19196560716|+14087181396|+14085069784|+14152549391|+16509969693|+15612719211|+17869735014|+15624539140|+16506192664|+13107078969|+1 4152609007|+16263750676|+14079219866|+18187266581|+18312220104|+19312496956|+13105978645|+13106262124|+19493502980|+14084995523|+14153788924|+17816161660|+ 14156867366|+17816161660|+16507434738|+14153368700|+15105797189|+14153086768|+16506192665|+12068566868|+13105005463|+13035266649|+12133009127|+14156095557| +14084293828|+16507968877|+12135905745");
         View view = inflater.inflate(R.layout.friends_tab, container, false);
 
-        loadingIcon = (ProgressBar)view.findViewById(R.id.loadingIcon);
+        loadingIcon = (ProgressBar) view.findViewById(R.id.loadingIcon);
 
         container.setBackgroundColor(getResources().getColor(android.R.color.white));
 
@@ -103,43 +104,17 @@ public class FriendsTabBtnFragment extends Fragment implements UserFriendsProtoc
     }//]~*~~*~~*~~*~~*~~*~~*~~*~~·¯
 
     private static class ViewHolder {
-        ImageView imageView;
-        String imageURL;
-        Bitmap bitmap;
+        TextView lblName;
+        // TextView lblFollowers;
+        // ImageView imgFollowers;
+        ImageView imgAddOrCheck;
+        ImageView imgAvatarCheck;
+        ImageView imgAvatar;
     }
-
-
-    private class DownloadAsyncTask extends AsyncTask<ViewHolder, Void, ViewHolder> {
-
-        @Override
-        protected ViewHolder doInBackground(ViewHolder... params) {
-            //load image directly
-
-            ViewHolder viewHolder = params[0];
-            try {
-                URL imageURL = new URL(viewHolder.imageURL);
-                viewHolder.bitmap = BitmapFactory.decodeStream(imageURL.openStream());
-            } catch (Exception e) {
-                viewHolder.bitmap = null;
-            }
-
-            return viewHolder;
-        }
-
-        @Override
-        protected void onPostExecute(ViewHolder result) {
-            if (result.bitmap == null) {
-                result.imageView.setImageResource(R.drawable.friends_item_image_mask);
-            } else {
-                result.imageView.setImageBitmap(result.bitmap);
-            }
-        }
-    }
-
 
     public void populate() {
         if (lv != null) {
-            adapter = new MyCustomAdapter(getActivity(), R.layout.friends_item, friends);
+            adapter = new MyCustomAdapter(getActivity().getBaseContext(), friends);
             lv.post(new Runnable() {
                 public void run() {
                     lv.setAdapter(adapter);
@@ -155,10 +130,14 @@ public class FriendsTabBtnFragment extends Fragment implements UserFriendsProtoc
         }
     }
 
-    private class MyCustomAdapter extends ArrayAdapter<Friend> {
+    private class MyCustomAdapter extends BaseAdapter {
 
-        public MyCustomAdapter(Context context, int textViewResourceId, List<Friend> list) {
-            super(context, textViewResourceId, list);
+        private LayoutInflater layoutInflater;
+        private ArrayList<Friend> listData;
+
+        public MyCustomAdapter(Context context, ArrayList<Friend> listData) {
+            this.listData = listData;
+            layoutInflater = LayoutInflater.from(context);
         }
 
         @Override
@@ -171,6 +150,15 @@ public class FriendsTabBtnFragment extends Fragment implements UserFriendsProtoc
             return friends.size();
         }
 
+        @Override
+        public Object getItem(int position) {
+            return listData.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
 
         @Override
         public View getView(final int position, View convertView, ViewGroup parent) {
@@ -181,53 +169,132 @@ public class FriendsTabBtnFragment extends Fragment implements UserFriendsProtoc
                 convertView = inflater.inflate(R.layout.friends_item, parent, false);
 
                 viewHolder = new ViewHolder();
-                viewHolder.imageView = (ImageView)convertView.findViewById(R.id.imgAvatar);
+                viewHolder.imgAvatar = (ImageView) convertView.findViewById(R.id.imgAvatar);
+                // viewHolder.lblFollowers = (TextView) convertView.findViewById(R.id.lblFollowers);
+                viewHolder.lblName = (TextView) convertView.findViewById(R.id.lblName);
+                //viewHolder.imgFollowers = (ImageView) convertView.findViewById(R.id.imgFollowers);
+                viewHolder.imgAddOrCheck = (ImageView) convertView.findViewById(R.id.imgAddOrCheck);
+                viewHolder.imgAvatarCheck = (ImageView) convertView.findViewById(R.id.imgAvatarCheck);
                 convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
             }
 
             Friend friend = friends.get(position);
 
-            viewHolder = (ViewHolder)convertView.getTag();
-            viewHolder.imageURL = friend.getAvatarUrl();
-            new DownloadAsyncTask().execute(viewHolder);
+            // viewHolder.lblFollowers.setText(String.valueOf(friend.getFollowers()));
+            viewHolder.lblName.setText(friend.getUsername());
 
-
-            // TextView lblFollowers = (TextView) convertView.findViewById(R.id.lblFollowers);
-            TextView lblName = (TextView) convertView.findViewById(R.id.lblName);
-            // ImageView imgFollowers = (ImageView) convertView.findViewById(R.id.imgFollowers);
-            ImageView imgAddOrCheck = (ImageView) convertView.findViewById(R.id.imgAddOrCheck);
-            ImageView imgAvatarCheck = (ImageView) convertView.findViewById(R.id.imgAvatarCheck);
-
-            // lblFollowers.setText(String.valueOf(friend.getFollowers()));
-            lblName.setText(friend.getUsername());
-
-           if (friend.getState() == 1) {
-                imgAddOrCheck.setBackgroundResource(R.drawable.green_selection_dot);
-                imgAvatarCheck.setVisibility(View.VISIBLE);
+            if (friend.getState() == 1) {
+                viewHolder.imgAddOrCheck.setBackgroundResource(R.drawable.green_selection_dot);
+                viewHolder.imgAvatarCheck.setVisibility(View.VISIBLE);
             } else {
-                imgAddOrCheck.setBackgroundResource(R.drawable.gray_selection_dot);
-                imgAvatarCheck.setVisibility(View.INVISIBLE);
+                viewHolder.imgAddOrCheck.setBackgroundResource(R.drawable.gray_selection_dot);
+                viewHolder.imgAvatarCheck.setVisibility(View.INVISIBLE);
             }
 
             /*if (friend.getFollowers() > 0){
-                imgFollowers.setBackgroundResource(R.drawable.verify_arrow_green);
-                lblFollowers.setTextColor(Color.GREEN);
+                viewHolder.imgFollowers.setBackgroundResource(R.drawable.verify_arrow_green);
+                viewHolder.lblFollowers.setTextColor(Color.GREEN);
             }else{
-                imgFollowers.setBackgroundResource(R.drawable.verify_arrow_grey);
-                lblFollowers.setTextColor(Color.LTGRAY);
+                viewHolder.imgFollowers.setBackgroundResource(R.drawable.verify_arrow_grey);
+                viewHolder.lblFollowers.setTextColor(Color.LTGRAY);
             }*/
+
+
+            if (viewHolder.imgAvatar != null) {
+                new ImageDownloaderTask(viewHolder.imgAvatar).execute(friend.getAvatarUrl());
+            }
 
             return convertView;
         }
     }
 
-    public void didReceiveFriendsList(FriendsViewData friendsViewData){
-        friends =  friendsViewData.getFriends();
+    public void didReceiveFriendsList(FriendsViewData friendsViewData) {
+        friends = friendsViewData.getFriends();
         owner = friendsViewData.getOwner();
         adapter.notifyDataSetChanged();
     }
-    public void didReceiveFriendsListError(String errorMessage){
+
+    public void didReceiveFriendsListError(String errorMessage) {
         System.err.println(errorMessage);
+    }
+
+    class ImageDownloaderTask extends AsyncTask<String, Void, Bitmap> {
+        private final WeakReference<ImageView> imageViewReference;
+
+        public ImageDownloaderTask(ImageView imageView) {
+            imageViewReference = new WeakReference<ImageView>(imageView);
+        }
+
+        @Override
+        // Actual download method, run in the task thread
+        protected Bitmap doInBackground(String... params) {
+            // params comes from the execute() call: params[0] is the url.
+            return downloadBitmap(params[0]);
+        }
+
+        @Override
+        // Once the image is downloaded, associates it to the imageView
+        protected void onPostExecute(Bitmap bitmap) {
+            if (isCancelled()) {
+                bitmap = null;
+            }
+
+            if (imageViewReference != null) {
+                ImageView imageView = imageViewReference.get();
+                if (imageView != null) {
+
+                    if (bitmap != null) {
+                        imageView.setImageBitmap(bitmap);
+                    } else {
+                        imageView.setImageDrawable(imageView.getContext().getResources()
+                                .getDrawable(R.drawable.friends_item_image_mask));
+                    }
+                }
+
+            }
+        }
+
+        Bitmap downloadBitmap(String url) {
+            final AndroidHttpClient client = AndroidHttpClient.newInstance("Android");
+            final HttpGet getRequest = new HttpGet(url);
+            try {
+                HttpResponse response = client.execute(getRequest);
+                final int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode != HttpStatus.SC_OK) {
+                    Log.w("ImageDownloader", "Error " + statusCode
+                            + " while retrieving bitmap from " + url);
+                    return null;
+                }
+
+                final HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = entity.getContent();
+                        final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        return bitmap;
+                    } finally {
+                        if (inputStream != null) {
+                            inputStream.close();
+                        }
+                        entity.consumeContent();
+                    }
+                }
+            } catch (Exception e) {
+                // Could provide a more explicit error message for IOException or
+                // IllegalStateException
+                getRequest.abort();
+                Log.w("ImageDownloader", "Error while retrieving bitmap from " + url);
+            } finally {
+                if (client != null) {
+                    client.close();
+                }
+            }
+            return null;
+        }
+
     }
 
 }
