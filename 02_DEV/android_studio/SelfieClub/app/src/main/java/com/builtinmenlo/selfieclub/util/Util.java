c8cf3f3bd74f296a260ea0571f7e24b0eaa249f5
Leonardo Ortiz <Leonardo@Leonardos-MacBook-Pro.local>
2014-06-25 19:48:06 -0600

    Added getUDID method

---
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/util/Util.java
@@ -0,0 +1,23 @@
+package com.builtinmenlo.selfieclub.util;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
+
+import java.util.UUID;
+
+/**
+ * Created by Leonardo on 6/25/14.
+ */
+public class Util {
+
+    public static String getUDID(Context context){
+        final TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        final String tmDevice, tmSerial, androidId;
+        tmDevice = "" + tm.getDeviceId();
+        tmSerial = "" + tm.getSimSerialNumber();
+        androidId = "" + android.provider.Settings.Secure.getString(context.getContentResolver(), android.provider.Settings.Secure.ANDROID_ID);
+        UUID deviceUuid = new UUID(androidId.hashCode(), ((long)tmDevice.hashCode() << 32) | tmSerial.hashCode());
+        String deviceId = deviceUuid.toString();
+        return deviceId;
+    }
+}
