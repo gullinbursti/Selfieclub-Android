50e299fc4c56081706394f9eafff350cc4dd474d
JavierCarvajal <javiercz@gmail.com>
2014-07-28 01:46:57 -0600

    Adding camera step 2 view

---
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/fragments/CameraStep2Fragment.java
@@ -0,0 +1,111 @@
+/**~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*._
+ /**~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~,
+ *
+ * @class SettingsTabBtnFragment
+ * @project SelfieClub
+ * @package com.builtinmenlo.selfieclub.fragments
+ *
+ * @author Matt.H <matt@builtinmenlo.com>
+ * @created 16-Apr-2014 @ 16:11
+ * @copyright (c) 2014 Built in Menlo, LLC. All rights reserved.
+ *
+/**~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~·'
+/**~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~**/
+
+
+package com.builtinmenlo.selfieclub.fragments;
+
+
+//] includes [!]>
+//]=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~.
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.builtinmenlo.selfieclub.R;
+import com.builtinmenlo.selfieclub.activity.CameraPreview;
+
+;
+//]~=~=~=~=~=~=~=~=~=~=~=~=~=~[]~=~=~=~=~=~=~=~=~=~=~=~=~=~[
+
+
+// <[!] class delaration [¡]>
+public class CameraStep2Fragment extends Fragment {
+//]~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~._
+
+    //] class properties ]>
+    //]=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~.
+    public static final String EXTRA_CODE = "country_code";
+
+    private Bundle bundle;
+    private ProgressBar loadingIcon;
+    //]~=~=~=~=~=~=~=~=~=~=~=~=~=~[]~=~=~=~=~=~=~=~=~=~=~=~=~=~[
+
+    // <*] class constructor [*>
+    public CameraStep2Fragment() {/*..\(^_^)/..*/}
+
+    //]~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=[>
+    //]~=~=~=~=~=~=~=~=~=[>
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        //]~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~._
+        View view = inflater.inflate(R.layout.first_run_country_selector, container, false);
+
+        bundle = getArguments();
+        byte[] avatarImage = null;
+        if (bundle!= null) {
+            avatarImage = bundle.getByteArray(CameraPreview.EXTRA_IMAGE);
+        }
+
+        ImageView avatar = (ImageView) view.findViewById(R.id.imgAvatar);
+        if (avatarImage != null){
+            Bitmap bmp = BitmapFactory.decodeByteArray(avatarImage, 0, avatarImage.length);
+            avatar.setImageBitmap(bmp);
+        }
+
+        final ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setTitle("How do I feel?");
+        actionBar.setBackgroundDrawable(new ColorDrawable());
+
+        loadingIcon = (ProgressBar) view.findViewById(R.id.loadingIcon);
+
+        container.setBackgroundColor(getResources().getColor(android.R.color.white));
+
+        hideKeyboard(view);
+        return view;
+    }//]~*~~*~~*~~*~~*~~*~~*~~*~~·¯
+
+
+    private void hideKeyboard(View v) {
+        InputMethodManager keyboard = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        keyboard.hideSoftInputFromWindow(v.getWindowToken(), 0);
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        //]~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~._
+        super.onCreate(savedInstanceState);
+    }//]~*~~*~~*~~*~~*~~*~~*~~*~~·¯
+
+    public void onAttach(Activity activity) {
+        //]~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~._
+        super.onAttach(activity);
+    }//]~*~~*~~*~~*~~*~~*~~*~~*~~·¯
+
+    public void onDetach() {
+        //]~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~._
+        super.onDetach();
+    }//]~*~~*~~*~~*~~*~~*~~*~~*~~·¯
+
+}
