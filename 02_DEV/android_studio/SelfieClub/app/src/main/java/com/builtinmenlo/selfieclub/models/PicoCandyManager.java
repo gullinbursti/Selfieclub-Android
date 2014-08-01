e87328dc91076fee766add9c384910883fbb136a
Leonardo Ortiz <Leonardo@Leonardos-MacBook-Pro.local>
2014-08-01 13:39:35 -0600

    Added thread safe execution block for PCContentGroup

--- 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/models/PicoCandyManager.java
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/models/PicoCandyManager.java
@@ -8,22 +8,42 @@ import com.picocandy.android.candystore.PCContentHelper;
 import com.picocandy.android.data.PCContentGroup;
 import com.picocandy.android.user.PicoUser;
 
+import java.util.ArrayList;
+
 /**
  * Created by Leonardo on 7/31/14.
  */
 public class PicoCandyManager {
 
-    public static void registerApp(Context context){
+    public void registerApp(Context context){
         PicoManager picoManager = PicoManager.getInstance();
         picoManager.registerStore(context, Constants.PICOCANDY_APP_ID,Constants.PICOCANDY_API_KEY);
     }
 
-    public static PicoUser getCurrentUser(){
+    public PicoUser getCurrentUser(){
         return PicoUser.currentUser();
     }
 
-    public static PCContentGroup getContentGroups(String ids){
+    public void getContentGroups(ArrayList<String>ids){
         PCContentHelper contentHelper = new PCContentHelper();
-        contentHelper.fetchContentGroup();
+        synchronized (this){
+            for(int i=0;i<ids.size();i++){
+                contentHelper.fetchContentGroup(ids.get(i),new PCContentHelper.FetchContentGroupListener() {
+                    @Override
+                    public void onSuccess(PCContentGroup pcContentGroup) {
+
+                    }
+
+                    @Override
+                    public void onFail(String s) {
+
+                    }
+                });
+            }
+        }
+
+
+
+
     }
 }
