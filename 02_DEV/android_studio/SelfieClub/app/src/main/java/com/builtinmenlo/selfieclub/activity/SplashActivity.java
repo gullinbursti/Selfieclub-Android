cda113c523fb38295dfce9e4543219086befb1c1
JavierCarvajal <javiercz@gmail.com>
2014-08-16 10:03:01 -0600

    Removing flashing screen with title

---
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/activity/SplashActivity.java
@@ -0,0 +1,62 @@
+/**~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*._
+ /**~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~,
+ *
+ * @class MainActivity
+ * @project SelfieClub
+ * @package com.builtinmenlo.selfieclub.activity
+ *
+ * @author Matt.H <matt@builtinmenlo.com>
+ * @created 16-Apr-2014 @ 11:50
+ * @copyright (c) 2014 Built in Menlo, LLC. All rights reserved.
+ *
+/**~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~·'
+/**~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~*~~**/
+
+
+package com.builtinmenlo.selfieclub.activity;
+
+
+//] includes [!]>
+//]=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~.
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.builtinmenlo.selfieclub.R;
+//]~=~=~=~=~=~=~=~=~=~=~=~=~=~[]~=~=~=~=~=~=~=~=~=~=~=~=~=~[
+
+
+// <[!] class delaration [¡]>
+public class SplashActivity extends Activity {
+
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 300;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
