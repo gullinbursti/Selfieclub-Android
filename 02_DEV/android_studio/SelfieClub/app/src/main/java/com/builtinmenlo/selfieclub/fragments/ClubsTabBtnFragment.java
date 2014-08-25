5e944fe8e7534195b6ceae5a8a2d9366ede4cbff
JavierCarvajal <javiercz@gmail.com>
2014-08-24 22:50:53 -0600

    Fixing clubs dialogs

--- 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/fragments/ClubsTabBtnFragment.java
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/fragments/ClubsTabBtnFragment.java
@@ -51,6 +51,9 @@ import com.builtinmenlo.selfieclub.util.Util;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -61,7 +64,7 @@ import java.util.List;
 
 
 // <[!] class declaration [¡]>
-public class ClubsTabBtnFragment extends Fragment implements UserClubsProtocol,SCDialogProtocol,ClubInviteProtocol {
+public class ClubsTabBtnFragment extends Fragment implements UserClubsProtocol, SCDialogProtocol, ClubInviteProtocol {
 
     private GridView gridClubs;
     private ProgressBar loadingIcon;
@@ -72,7 +75,6 @@ public class ClubsTabBtnFragment extends Fragment implements UserClubsProtocol,S
     private static String RECEIVE_CLUBS_ERROR_TAG = "error_retrieving_clubs";
     private static String NO_CLUBS_ERROR_TAG = "no_clubs";
     private static String WOULD_YOU_JOIN_TAG = "would_you_join";
-
 
 
     public ClubsTabBtnFragment() {/*..\(^_^)/..*/}
@@ -89,8 +91,8 @@ public class ClubsTabBtnFragment extends Fragment implements UserClubsProtocol,S
 
         UserManager userManager = new UserManager();
         ApplicationManager applicationManager = new ApplicationManager(getActivity());
-        //userManager.requestUserClubs(this, applicationManager.getUserId());
-        userManager.requestUserClubs(this, "3482");
+        userManager.requestUserClubs(this, applicationManager.getUserId());
+        //userManager.requestUserClubs(this, "3509");
 
         return view;
     }
@@ -161,18 +163,23 @@ public class ClubsTabBtnFragment extends Fragment implements UserClubsProtocol,S
                         dialog.setNeutralButtonTitle(getString(R.string.ok_button_title));
                         dialog.setMessage(getString(R.string.label_coming_soon));
                         dialog.showTwoButtons = false;
-                        dialog.show(getFragmentManager(),"");
+                        dialog.show(getFragmentManager(), "");
                     } else {
                         ApplicationManager applicationManager = new ApplicationManager(getActivity());
                         Club clubSelected = clubs.get(position - 1);
                         boolean isMember = (clubSelected.getClubOwner().getUserId() == applicationManager.getUserId());
                         if (!isMember) {
                             for (int i = 0; i < clubSelected.getClubMembers().length(); i++) {
-                                //User
-                                //if
+                                try {
+                                    JSONObject member = clubSelected.getClubMembers().getJSONObject(i);
+                                    if (applicationManager.getUserId().equals(member.getString("id"))) {
+                                        isMember = true;
+                                    }
+                                } catch (JSONException e) {
+                                }
                             }
                         }
-                        if (isMember) {
+                        if (!isMember) {
                             showInviteDialog(clubSelected);
                         } else {
                             SCDialog scdialog = new SCDialog();
@@ -227,51 +234,50 @@ public class ClubsTabBtnFragment extends Fragment implements UserClubsProtocol,S
                 convertView.setTag(viewHolder);
             } else {
                 //if (convertView == null) {
-                    LayoutInflater inflater = getActivity().getLayoutInflater();
-                    convertView = inflater.inflate(R.layout.clubs_item, parent, false);
-                    viewHolder = new ViewHolder();
-                    viewHolder.imageView = (ImageView) convertView.findViewById(R.id.imgClub);
+                LayoutInflater inflater = getActivity().getLayoutInflater();
+                convertView = inflater.inflate(R.layout.clubs_item, parent, false);
+                viewHolder = new ViewHolder();
+                viewHolder.imageView = (ImageView) convertView.findViewById(R.id.imgClub);
                     /*Bitmap image = Bitmap.createBitmap(96, 96, Bitmap.Config.ARGB_8888);
                     image.eraseColor(Color.TRANSPARENT);
                     viewHolder.imageView.setImageBitmap(image);*/
-                    viewHolder.loadingImage = (ProgressBar) convertView.findViewById(R.id.loadingImage);
-                    viewHolder.loadingImage.setVisibility(View.VISIBLE);
-                    convertView.setTag(viewHolder);
+                viewHolder.loadingImage = (ProgressBar) convertView.findViewById(R.id.loadingImage);
+                viewHolder.loadingImage.setVisibility(View.VISIBLE);
+                convertView.setTag(viewHolder);
 
 
-                    Club club = clubs.get(position - 1);
-                    try{
-                        String clubImagePath = new URL(club.getClubImage()).getPath();
-                        String newClubImageUrl = "http://hotornot-challenges.s3.amazonaws.com"+clubImagePath;
-                        viewHolder.imageURL = newClubImageUrl;
+                Club club = clubs.get(position - 1);
+                try {
+                    String clubImagePath = new URL(club.getClubImage()).getPath();
+                    String newClubImageUrl = "http://hotornot-challenges.s3.amazonaws.com" + clubImagePath;
+                    viewHolder.imageURL = newClubImageUrl;
+                } catch (Exception e) {
+                    viewHolder.imageURL = "";
+                    Log.w("ClubsTabBtnFragment", "Error parsing the club image id");
+                }
+                ;
+
+
+                //viewHolder = (ViewHolder) convertView.getTag();
+                viewHolder.imageURL = club.getClubImage();
+
+                Picasso.with(getActivity()).load(viewHolder.imageURL).into(viewHolder.imageView, new Callback() {
+
+                    @Override
+                    public void onSuccess() {
+                        viewHolder.loadingImage.setVisibility(View.INVISIBLE);
                     }
-                    catch (Exception e){
-                        viewHolder.imageURL = "";
-                        Log.w("ClubsTabBtnFragment","Error parsing the club image id");
-                    };
 
+                    @Override
+                    public void onError() {
+                        viewHolder.imageView.setImageResource(R.drawable.default_club_cover);
+                        viewHolder.loadingImage.setVisibility(View.INVISIBLE);
+                    }
+                });
 
+                TextView lblText = (TextView) convertView.findViewById(R.id.lblClubName);
 
-                    //viewHolder = (ViewHolder) convertView.getTag();
-                    viewHolder.imageURL = club.getClubImage();
-
-                    Picasso.with(getActivity()).load(viewHolder.imageURL).into(viewHolder.imageView, new Callback() {
-
-                        @Override
-                        public void onSuccess() {
-                            viewHolder.loadingImage.setVisibility(View.INVISIBLE);
-                        }
-
-                        @Override
-                        public void onError() {
-                            viewHolder.imageView.setImageResource(R.drawable.default_club_cover);
-                            viewHolder.loadingImage.setVisibility(View.INVISIBLE);
-                        }
-                    });
-
-                    TextView lblText = (TextView) convertView.findViewById(R.id.lblClubName);
-
-                    lblText.setText(club.getClubName());
+                lblText.setText(club.getClubName());
                 //}
             }
             return convertView;
@@ -281,7 +287,7 @@ public class ClubsTabBtnFragment extends Fragment implements UserClubsProtocol,S
 
     public void didReceiveUserClubs(ArrayList<Club> userClubs) {
         loadingIcon.setVisibility(View.INVISIBLE);
-        if (userClubs.size() < 1 ) {
+        if (userClubs.size() < 1) {
             getActivity().findViewById(R.id.txtError).setVisibility(View.VISIBLE);
         } else {
             Log.i(this.getActivity().getClass().getName(), userClubs.toString());
@@ -301,61 +307,61 @@ public class ClubsTabBtnFragment extends Fragment implements UserClubsProtocol,S
     }
 
     //Dialog protocol
-    public void didClickedButton(String dialogTag, int buttonIndex){
-        if(dialogTag.equalsIgnoreCase(INVITE_RANDOM_FRIENDS_TAG)){
-            if(buttonIndex==1){
+    public void didClickedButton(String dialogTag, int buttonIndex) {
+        if (dialogTag.equalsIgnoreCase(INVITE_RANDOM_FRIENDS_TAG)) {
+            if (buttonIndex == 1) {
                 inviteRandomFriends();
             }
         }
     }
     //Invite 10 friends club
 
-    public void showInviteDialog(Club club){
+    public void showInviteDialog(Club club) {
         selectedClub = club;
         String message = getResources().getString(R.string.invite_random_friends_dialog);
         SCDialog dialog = new SCDialog();
         dialog.setScDialogProtocol(this);
-        dialog.showTwoButtons=true;
+        dialog.showTwoButtons = true;
         dialog.setMessage(message);
         dialog.setPositiveButtonTitle(getResources().getString(R.string.yes_button_title));
         dialog.setNegativeButtonTitle(getResources().getString(R.string.no_button_title));
-        dialog.show(getFragmentManager(),INVITE_RANDOM_FRIENDS_TAG);
+        dialog.show(getFragmentManager(), INVITE_RANDOM_FRIENDS_TAG);
     }
 
-    public void inviteRandomFriends(){
+    public void inviteRandomFriends() {
         ApplicationManager applicationManager = new ApplicationManager(this.getActivity());
         String userId = applicationManager.getUserId();
         String clubId = selectedClub.getClubId();
         PhoneManager phoneManager = new PhoneManager();
-        ArrayList<HashMap<String,String>> contactsList = phoneManager.getContacts(this.getActivity().getContentResolver());
-        HashMap<String,String> contact;
-        ArrayList<HashMap<String,String>> friendsToInvite = new ArrayList<HashMap<String, String>>();
+        ArrayList<HashMap<String, String>> contactsList = phoneManager.getContacts(this.getActivity().getContentResolver());
+        HashMap<String, String> contact;
+        ArrayList<HashMap<String, String>> friendsToInvite = new ArrayList<HashMap<String, String>>();
         ArrayList<String> registeredFriends = new ArrayList<String>();
-        if(contactsList.size()>Constants.NUMBER_OF_RANDOM_FRIENDS){
-            int[] randomNumbers = Util.randomNumbers(Constants.NUMBER_OF_RANDOM_FRIENDS,0,contactsList.size());
-            for(int i=0;i<randomNumbers.length;i++){
+        if (contactsList.size() > Constants.NUMBER_OF_RANDOM_FRIENDS) {
+            int[] randomNumbers = Util.randomNumbers(Constants.NUMBER_OF_RANDOM_FRIENDS, 0, contactsList.size());
+            for (int i = 0; i < randomNumbers.length; i++) {
                 contact = contactsList.get(randomNumbers[i]);
                 friendsToInvite.add(contact);
             }
-        }
-        else {
-            for (int i=0;i<contactsList.size();i++){
+        } else {
+            for (int i = 0; i < contactsList.size(); i++) {
                 contact = contactsList.get(i);
                 friendsToInvite.add(contact);
             }
         }
 
         ClubManager clubManager = new ClubManager();
-        clubManager.sendClubInvite(this,userId,clubId,registeredFriends,friendsToInvite);
+        clubManager.sendClubInvite(this, userId, clubId, registeredFriends, friendsToInvite);
 
 
     }
 
-    public void didSendCubInvite(Boolean response){
+    public void didSendCubInvite(Boolean response) {
         Util.playDefaultNotificationSound(getActivity().getApplicationContext());
     }
-    public void didFailSendingClubInvite(String errorMessage){
-        Log.w(this.getClass().getName(),"Failed inviting friends");
+
+    public void didFailSendingClubInvite(String errorMessage) {
+        Log.w(this.getClass().getName(), "Failed inviting friends");
     }
 
 }
--- 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/models/UserManager.java
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/models/UserManager.java
@@ -119,6 +119,7 @@ public class UserManager
     public void requestUserClubs(final UserClubsProtocol userClubsProtocol, String userId){
         AsyncHttpClient client = new AsyncHttpClient();
         HashMap<String, String> data = new HashMap<String, String>();
+        //data.put("userID","155489");
         data.put("userID",userId);
         RequestParams requestParams = new RequestParams(data);
         client.post(Constants.API_ENDPOINT+Constants.GET_USERCLUBS_PATH,requestParams,new JsonHttpResponseHandler() {
