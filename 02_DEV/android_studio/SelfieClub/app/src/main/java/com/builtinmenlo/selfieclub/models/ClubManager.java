c0cfb2fb7680e2e3b4c13f881cc11c076bae9f51
Leonardo Ortiz <Leonardo@Leonardos-MacBook-Pro.local>
2014-08-28 14:44:32 -0600

    Removed non number chars from phones in invite

--- 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/models/ClubManager.java
+++ 02_DEV/android_studio/SelfieClub/app/src/main/java/com/builtinmenlo/selfieclub/models/ClubManager.java
@@ -103,11 +103,12 @@ public class ClubManager {
         String nonUsersStr = "";
         if (nonUsers.size() > 0) {
             HashMap<String, String> buffer = nonUsers.get(0);
-            nonUsersStr = buffer.get("name") + ":::" + buffer.get("number") + "::";
+            nonUsersStr = buffer.get("name") + ":::" + buffer.get("number").replaceAll("[^0-9]", "") + "::";
             if (nonUsers.size() > 1) {
                 for (int i = 1; i < nonUsers.size(); i++) {
                     buffer = nonUsers.get(i);
-                    nonUsersStr = nonUsersStr + "|||" + buffer.get("name") + ":::" + buffer.get("number") + "::";
+
+                    nonUsersStr = nonUsersStr + "|||" + buffer.get("name") + ":::" + buffer.get("number").replaceAll("[^0-9]", "") + "::";
                 }
             }
         }
